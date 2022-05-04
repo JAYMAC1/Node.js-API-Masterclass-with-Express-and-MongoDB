c49aa10b2be729afb5d0992d27cec90350b9f3bb
James McNeil <mcneil_james@hotmail.com>
2022-05-04 21:09:49 +0100

    moved routes to bootcamps routes

--- server.js
+++ server.js
@@ -1,29 +1,11 @@
 const express = require('express') // import express
 const dotenv = require('dotenv').config({ path: './config/config.env' }) // import and ref .env config file (environmental variables)
-
+const bootcamps = require('./routes/bootcamps')
 const PORT = process.env.PORT || 5000 // Get port from .env
 
 const app = express() // create instance of express 'app'
 
-app.get('/api/v1/bootcamps', (req, res) => {
-  res.status(200).json({ success: true, msg: 'Show all bootcamps' })
-})
-app.get('/api/v1/bootcamps/:id', (req, res) => {
-  res.status(200).json({ success: true, msg: `Get bootcamp ${req.params.id}` })
-})
-app.post('/api/v1/bootcamps', (req, res) => {
-  res.status(200).json({ success: true, msg: 'Create new bootcamps' })
-})
-app.put('/api/v1/bootcamps/:id', (req, res) => {
-  res
-    .status(200)
-    .json({ success: true, msg: `Update bootcamp ${req.params.id}` })
-})
-app.delete('/api/v1/bootcamps/:id', (req, res) => {
-  res
-    .status(200)
-    .json({ success: true, msg: `Delete bootcamp ${req.params.id}` })
-})
+app.use('/api/v1/bootcamps', bootcamps)
 
 app.listen(
   PORT,
