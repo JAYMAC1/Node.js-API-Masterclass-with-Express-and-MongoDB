702197b7b0287e9c3f6727f1325b3de762e718d7
James McNeil <mcneil_james@hotmail.com>
2022-05-04 21:12:39 +0100

    added comments

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const express = require('express') // import express
 const dotenv = require('dotenv').config({ path: './config/config.env' }) // import and ref .env config file (environmental variables)
-const bootcamps = require('./routes/bootcamps')
+const bootcamps = require('./routes/bootcamps') // import bootcamp routers file
 const PORT = process.env.PORT || 5000 // Get port from .env
 
 const app = express() // create instance of express 'app'
 
-app.use('/api/v1/bootcamps', bootcamps)
+app.use('/api/v1/bootcamps', bootcamps) // mount bootcamp router
 
 app.listen(
   PORT,
