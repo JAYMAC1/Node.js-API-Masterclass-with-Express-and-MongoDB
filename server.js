105b7d6005be4df1fa1f9572bc4009d75147be12
James McNeil <mcneil_james@hotmail.com>
2022-05-04 20:57:51 +0100

    added test bootcamp routes

--- server.js
+++ server.js
@@ -5,6 +5,23 @@ const PORT = process.env.PORT || 5000 // Get port from .env
 
 const app = express() // create instance of express 'app'
 
+app.get('/api/v1/bootcamps', (req, res) => {
+  res.status(200).json({ success: true, msg: 'Show all bootcamps' })
+})
+app.post('/api/v1/bootcamps', (req, res) => {
+  res.status(200).json({ success: true, msg: 'Create new bootcamps' })
+})
+app.put('/api/v1/bootcamps/:id', (req, res) => {
+  res
+    .status(200)
+    .json({ success: true, msg: `Update bootcamp ${req.params.id}` })
+})
+app.delete('/api/v1/bootcamps/:id', (req, res) => {
+  res
+    .status(200)
+    .json({ success: true, msg: `Delete bootcamp ${req.params.id}` })
+})
+
 app.listen(
   PORT,
   console.log(`Server running in ${process.env.NODE_ENV} mode on PORT: ${PORT}`)
