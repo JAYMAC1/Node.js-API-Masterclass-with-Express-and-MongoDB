fcedc6f714d800166c50718a886ce0ad04284e87
James McNeil <mcneil_james@hotmail.com>
2022-05-05 18:23:39 +0100

    removed next from controller arguments

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -1,45 +1,40 @@
 // @desc    Fetch all bootcamps
 // @route   GET /api/v1/bootcamps
 // @access  Public
-const listBootcamps = (req, res, next) => {
+const listBootcamps = (req, res) => {
   res.status(200).json({ success: true, msg: 'Show all bootcamps' })
-  next
 }
 
 // @desc    Fetch single bootcamp
 // @route   GET /api/v1/bootcamps/:id
 // @access  Public
-const getBootcamp = (req, res, next) => {
+const getBootcamp = (req, res) => {
   res.status(200).json({ success: true, msg: `Get bootcamp ${req.params.id}` })
-  next
 }
 
 // @desc    Create a bootcamp
 // @route   POST /api/v1/bootcamps
 // @access  Private
-const createBootcamp = (req, res, next) => {
+const createBootcamp = (req, res) => {
   res.status(200).json({ success: true, msg: 'Create new bootcamp' })
-  next
 }
 
 // @desc    Update single bootcamp
 // @route   PUT /api/v1/bootcamps/:id
 // @access  Private
-const updateBootcamp = (req, res, next) => {
+const updateBootcamp = (req, res) => {
   res
     .status(200)
     .json({ success: true, msg: `Update bootcamp ${req.params.id}` })
-  next
 }
 
 // @desc    Delete a bootcamp
 // @route   DELETE /api/v1/bootcamps/:id
 // @access  Private
-const deleteBootcamp = (req, res, next) => {
+const deleteBootcamp = (req, res) => {
   res
     .status(200)
     .json({ success: true, msg: `Delete bootcamp ${req.params.id}` })
-  next
 }
 
 module.exports = {
