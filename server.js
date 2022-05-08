9fc921381b90766b14f068b00dab32a94fb924c2
James McNeil <mcneil_james@hotmail.com>
2022-05-08 21:27:17 +0100

    import logger to server.js

--- server.js
+++ server.js
@@ -1,15 +1,10 @@
 const express = require('express') // import express
 const dotenv = require('dotenv').config({ path: './config/config.env' }) // import and ref .env config file (environmental variables)
+const { logger } = require('./middleware/logger')
 const bootcamps = require('./routes/bootcamps') // import bootcamp routers file
 const PORT = process.env.PORT || 5000 // Get port from .env
 
 const app = express() // create instance of express 'app'
-
-const logger = (req, res, next) => {
-  req.hello = 'Hello World'
-  console.log('Middleware ran')
-  next()
-}
 
 app.use(logger)
 app.use('/api/v1/bootcamps', bootcamps) // mount bootcamp router
