db9dd4296fd3d3cf546a029e1e48abab83aaf78b
James McNeil <mcneil_james@hotmail.com>
2022-05-09 21:01:50 +0100

    initialised morgan dev instance to log output to the console only when running in dev mode

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 const express = require('express') // import express
 const dotenv = require('dotenv').config({ path: './config/config.env' }) // import and ref .env config file (environmental variables)
-const logger = require('./middleware/logger')
+const morgan = require('morgan')
 const bootcamps = require('./routes/bootcamps') // import bootcamp routers file
 const PORT = process.env.PORT || 5000 // Get port from .env
 
 const app = express() // create instance of express 'app'
 
-app.use(logger)
+// Dev loggin middleware
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'))
+}
+
 app.use('/api/v1/bootcamps', bootcamps) // mount bootcamp router
 
 app.listen(
